ae4511300433a42166079cd66687d424cf726631
Andydas <48646883+Andydas@users.noreply.github.com>
2022-06-15 16:31:46 +0200

    transfer z gitlabu server
    transfer servera z gitlabu

---
+++ JumpenoWebassembly/Server/Logging/LogEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JumpenoWebassembly.Server.Logging
+{
+    public class LogEvents
+    {
+        public const int StartGame =                    1000;
+        public const int EndGame =                      1001;
+
+        public const int GeneratePlayerPosition =       2000;
+        public const int GetPlayerPosition =            2001;
+
+        public const int NotifyCollision =              3001;
+        public const int NotifyEliminationByPlayer =    3002;
+        public const int NotifyEliminationByMapShrink = 3003;
+    }
+}
