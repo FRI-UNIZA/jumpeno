e0ec3a6993e0d5616dd25214970ba5cb1bf41729
andydas <drzik.andrej@gmail.com>
2022-07-10 11:29:33 +0200

    Vytvorena trieda CustomErrorBoundary.cs

---
+++ JumpenoWebassembly/Client/ExceptionHandling/CustomErrorBoundary.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace JumpenoWebassembly.Client.ExceptionHandling
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        [Inject]
+        private IWebAssemblyHostEnvironment env { get; set; }
+        private NavigationManager NavigationManager { get; set; }
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            Console.WriteLine("SAHFASHFDKJLASHFJK");
+            if (env.IsDevelopment())
+            {
+                return base.OnErrorAsync(exception);
+            }
+            return Task.CompletedTask;
+        }
+
+        public void Clear()
+        {
+            NavigationManager.NavigateTo("/");
+            Recover();
+        }
+    }
+}
+        
