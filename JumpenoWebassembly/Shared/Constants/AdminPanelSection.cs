1c547d4472901ff42aecd4f89d3f60a6a83009ea
andydas <drzik.andrej@gmail.com>
2022-07-10 20:58:39 +0200

    Error logy prenesene na stranku admin panelu

---
+++ JumpenoWebassembly/Shared/Constants/AdminPanelSection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpenoWebassembly.Shared.Constants
+{
+    public class AdminPanelSection
+    {
+        public const int Games = 1;
+        public const int Monitor = 2;
+        public const int Errors = 3;
+    }
+}
