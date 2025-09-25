7c1c9fca17702c1633922f9a6b5c6abf40c429a1
Daniel Badušek <daniel.badusek@gmail.com>
2025-09-26 01:26:42 +0200

    [#80]: Change FIELD.EXISTS to getter

--- Jumpeno.Client/Services/Validation/Constants/Errors/FIELD.cs
+++ Jumpeno.Client/Services/Validation/Constants/Errors/FIELD.cs
@@ -10,5 +10,5 @@ public static class FIELD {
     public static TInfo MATCH(object value1, object value2) => new("Equal values of I18N{value1} and I18N{value2}", new() {{ "value1", value1 }, { "value2", value2 } });
     public static TInfo NOT_MATCH() => new("Not a match");
     public static TInfo NOT_MATCH(object value1, object value2) => new("Not a match of I18N{value1} and I18N{value2}", new() {{ "value1", value1 }, { "value2", value2 } });
-    public static TInfo EXISTS = new("Already exists");
+    public static TInfo EXISTS => new("Already exists");
 }
