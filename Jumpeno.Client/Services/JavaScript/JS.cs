b2fd56c71d3da7828f9d214c65e1f0ed568670b5
Daniel Badušek <daniel.badusek@gmail.com>
2025-09-26 04:37:43 +0200

    [#80]: JS Improvement

--- Jumpeno.Client/Services/JavaScript/JS.cs
+++ Jumpeno.Client/Services/JavaScript/JS.cs
@@ -3,6 +3,9 @@ namespace Jumpeno.Client.Services;
 #pragma warning disable CS8618
 
 public class JS {
+    // Constants --------------------------------------------------------------------------------------------------------------------------
+    private const string EVAL = "eval";
+
     // Attributes -------------------------------------------------------------------------------------------------------------------------
     private static IJSInProcessRuntime Runtime => (IJSInProcessRuntime)AppEnvironment.GetService<IJSRuntime>();
 
@@ -14,4 +17,13 @@ public class JS {
     public static T Invoke<T>(string identifier, params object?[]? args) => Runtime.Invoke<T>(identifier, args);
 
     public static async Task<T> InvokeAsync<T>(string identifier, params object?[]? args) => await Runtime.InvokeAsync<T>(identifier, args);
+
+    // Eval -------------------------------------------------------------------------------------------------------------------------------
+    public static void EvalVoid(string code) => Runtime.InvokeVoid(EVAL, code);
+
+    public static async Task EvalVoidAsync(string code) => await Runtime.InvokeVoidAsync(EVAL, code);
+
+    public static T Eval<T>(string code) => Runtime.Invoke<T>(EVAL, code);
+
+    public static async Task<T> EvalAsync<T>(string code) => await Runtime.InvokeAsync<T>(EVAL, code);
 }
