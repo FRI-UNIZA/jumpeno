2b82f175a7e9f2e590fa542d8372335e2b631268
Daniel Badušek <daniel.badusek@gmail.com>
2025-10-15 07:17:38 +0200

    [#75]: Wrong LocalStorage constant fix

--- Jumpeno.Client/Services/Storage/LocalStorage/LocalStorage.cs
+++ Jumpeno.Client/Services/Storage/LocalStorage/LocalStorage.cs
@@ -3,7 +3,7 @@ namespace Jumpeno.Client.Services;
 public static class LocalStorage {
     public static string Get(string key) => JS.Invoke<string>(JSLocalStorage.Get, key);
 
-    public static bool IsSet(string key) => JS.Invoke<string>(JSSessionStorage.Get, key) != null;
+    public static bool IsSet(string key) => JS.Invoke<string>(JSLocalStorage.Get, key) != null;
 
     public static void Set(string key, string value = "true") => JS.InvokeVoid(JSLocalStorage.Set, key, value);
 
